3e8aa32e9636bd9074813bc8621f5afafd0d3acf
KingJA <kingjavip@gmail.com>
2017-01-12 17:09:20 +0800

    finish category

--- src/main/java/com/kingja/springmvc/controller/MainController.java
+++ src/main/java/com/kingja/springmvc/controller/MainController.java
@@ -6,7 +6,6 @@ import com.kingja.springmvc.entity.Article;
 import com.kingja.springmvc.entity.Category;
 import com.kingja.springmvc.entity.Month;
 import com.kingja.springmvc.service.AdminService;
-import com.kingja.springmvc.util.HttpClientUtil;
 import com.kingja.springmvc.util.Page2;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -35,77 +34,14 @@ public class MainController {
     @Autowired
     AdminDao adminDao;
 
-    public static String decodeUnicode(String theString) {
-        char aChar;
-        int len = theString.length();
-        StringBuffer outBuffer = new StringBuffer(len);
-        for (int x = 0; x < len; ) {
-            aChar = theString.charAt(x++);
-            if (aChar == '\\') {
-                aChar = theString.charAt(x++);
-                if (aChar == 'u') {
-                    // Read the xxxx
-                    int value = 0;
-                    for (int i = 0; i < 4; i++) {
-                        aChar = theString.charAt(x++);
-                        switch (aChar) {
-                            case '0':
-                            case '1':
-                            case '2':
-                            case '3':
-                            case '4':
-                            case '5':
-                            case '6':
-                            case '7':
-                            case '8':
-                            case '9':
-                                value = (value << 4) + aChar - '0';
-                                break;
-                            case 'a':
-                            case 'b':
-                            case 'c':
-                            case 'd':
-                            case 'e':
-                            case 'f':
-                                value = (value << 4) + 10 + aChar - 'a';
-                                break;
-                            case 'A':
-                            case 'B':
-                            case 'C':
-                            case 'D':
-                            case 'E':
-                            case 'F':
-                                value = (value << 4) + 10 + aChar - 'A';
-                                break;
-                            default:
-                                throw new IllegalArgumentException(
-                                        "Malformed   \\uxxxx   encoding.");
-                        }
 
-                    }
-                    outBuffer.append((char) value);
-                } else {
-                    if (aChar == 't')
-                        aChar = '\t';
-                    else if (aChar == 'r')
-                        aChar = '\r';
-                    else if (aChar == 'n')
-                        aChar = '\n';
-                    else if (aChar == 'f')
-                        aChar = '\f';
-                    outBuffer.append(aChar);
-                }
-            } else
-                outBuffer.append(aChar);
-        }
-        return outBuffer.toString();
-    }
 
     @RequestMapping(value = "", method = RequestMethod.GET)
     public ModelAndView home() {
-        String result = HttpClientUtil.getInstance().sendHttpsGet("http://ip.taobao.com/service/getIpInfo.php?ip=221.131.238.146");
-        result = decodeUnicode(result);
-        logger.error("result:" + result);
+//        String result = HttpClientUtil.getInstance().sendHttpsGet("http://ip.taobao.com/service/getIpInfo.php?ip=221.131.238.146");
+//        IpInfo ipInfo = ConvertUtil.str2Entity(result, IpInfo.class);
+//        logger.error("ipInfo:" + ipInfo.getData().toString());
+
         ModelAndView modelAndView = new ModelAndView("/main/index");
         //1.获取文章列表
         Page2<Article> articlePage = adminService.getArticles(1, 5);
---
+++ src/main/java/com/kingja/springmvc/util/ConvertUtil.java
@@ -0,0 +1,90 @@
+package com.kingja.springmvc.util;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonSyntaxException;
+
+/**
+ * Description：TODO
+ * Create Time：2017/1/12 16:50
+ * Author:KingJA
+ * Email:kingjavip@gmail.com
+ */
+public class ConvertUtil {
+    public static <T> T str2Entity(String jsonString, Class<T> type) {
+
+        T t = null;
+        try {
+            Gson gson = new Gson();
+            t = gson.fromJson(jsonString, type);
+        } catch (JsonSyntaxException e) {
+            e.printStackTrace();
+        }
+        return t;
+    }
+
+    public static String decodeUnicode(String theString) {
+        char aChar;
+        int len = theString.length();
+        StringBuffer outBuffer = new StringBuffer(len);
+        for (int x = 0; x < len; ) {
+            aChar = theString.charAt(x++);
+            if (aChar == '\\') {
+                aChar = theString.charAt(x++);
+                if (aChar == 'u') {
+                    // Read the xxxx
+                    int value = 0;
+                    for (int i = 0; i < 4; i++) {
+                        aChar = theString.charAt(x++);
+                        switch (aChar) {
+                            case '0':
+                            case '1':
+                            case '2':
+                            case '3':
+                            case '4':
+                            case '5':
+                            case '6':
+                            case '7':
+                            case '8':
+                            case '9':
+                                value = (value << 4) + aChar - '0';
+                                break;
+                            case 'a':
+                            case 'b':
+                            case 'c':
+                            case 'd':
+                            case 'e':
+                            case 'f':
+                                value = (value << 4) + 10 + aChar - 'a';
+                                break;
+                            case 'A':
+                            case 'B':
+                            case 'C':
+                            case 'D':
+                            case 'E':
+                            case 'F':
+                                value = (value << 4) + 10 + aChar - 'A';
+                                break;
+                            default:
+                                throw new IllegalArgumentException(
+                                        "Malformed   \\uxxxx   encoding.");
+                        }
+
+                    }
+                    outBuffer.append((char) value);
+                } else {
+                    if (aChar == 't')
+                        aChar = '\t';
+                    else if (aChar == 'r')
+                        aChar = '\r';
+                    else if (aChar == 'n')
+                        aChar = '\n';
+                    else if (aChar == 'f')
+                        aChar = '\f';
+                    outBuffer.append(aChar);
+                }
+            } else
+                outBuffer.append(aChar);
+        }
+        return outBuffer.toString();
+    }
+}
